06de7b480d478a343d442241042222ec6a2522ec
Evan Overman <evanrileyoverman@gmail.com>
2022-10-28 15:02:37 -0700

    hopefully making minor performance improvements

--- macOS-dynamic-wallpaper-imitation.sh
+++ macOS-dynamic-wallpaper-imitation.sh
@@ -20,42 +20,45 @@ SUNSET="7.png"
 DUSK="8.png"
 NIGHT="2.png"
 
+CURRENT_FILE="$WALLPAPER_BASE_NAME$NIGHT"
+FILE="$WALLPAPER_BASE_NAME$NIGHT"
+
 while [ true ]; do
+	# Retreive the date.
 	TIME=$(date +%H)
 	echo "Hour is $TIME"
-	
+
+	# Set the FILE variable based on the time.
 	if (( 7 <= $TIME )) && (( $TIME < 9 )); then
 		FILE="$WALLPAPER_BASE_NAME$EARLY_SUNRISE"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	elif (( 9 <= $TIME )) && (( $TIME < 11 )); then
 		FILE="$WALLPAPER_BASE_NAME$LATE_SUNRISE"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	elif (( 11 <= $TIME )) && (( $TIME < 13 )); then
 		FILE="$WALLPAPER_BASE_NAME$DAYTIME"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	elif (( 13 <= $TIME )) && (( $TIME < 15 )); then
 		FILE="$WALLPAPER_BASE_NAME$EARLY_AFTERNOON"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	elif (( 15 <= $TIME )) && (( $TIME < 16 )); then
 		FILE="$WALLPAPER_BASE_NAME$LATE_AFTERNOON"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	elif (( 16 <= $TIME )) && (( $TIME < 17 )); then
 		FILE="$WALLPAPER_BASE_NAME$SUNSET"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	elif (( 17 <= $TIME )) && (( $TIME < 18 )); then
 		FILE="$WALLPAPER_BASE_NAME$DUSK"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
 	else
 		FILE="$WALLPAPER_BASE_NAME$NIGHT"
-		$WALLPAPER_SET_COMMAND "$FILE"
 		echo "Using $FILE"
+	fi
+	
+	# Prevent setting the wallpaper more than needed
+	if [[ CURRENT_FILE != FILE ]]; then
+		$WALLPAPER_SET_COMMAND "$FILE"
+		CURRENT_FILE=$FILE	
 	fi
 
 	# Sleep for one minute between iterations.
