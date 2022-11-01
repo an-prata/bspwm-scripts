29161d7591d34ae6022892fb2bd0ad94e3b5897c
Evan Overman <evanrileyoverman@gmail.com>
2022-11-01 16:07:09 -0700

    add window size for simplest-fetch

--- floating-window-default-sizes.sh
+++ floating-window-default-sizes.sh
@@ -3,4 +3,5 @@
 bspc rule -a "kitty" center=true rectangle=920x640+0+0
 bspc rule -a "Zathura" center=true rectangle=846x980+0+0
 bspc rule -a "miniplayer" center=true rectangle=486x732+0+0
+bspc rule -a "simplest-fetch" center=true rectangle=780x430+0+0
 
