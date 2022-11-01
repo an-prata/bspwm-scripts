e7d92cc9225482d988fe6576f22b79d5cc95579a
Evan Overman <evanrileyoverman@gmail.com>
2022-11-01 16:11:21 -0700

    fix spelling

--- get-default-window-state.sh
+++ get-default-window-state.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Intended for use as a polybar module in conjunction
-# with the ther scripts found in this directory.
+# with the other scripts found in this directory.
 
 bspc rule -l | grep "*:*:* => state=tiled" > /dev/null
 
