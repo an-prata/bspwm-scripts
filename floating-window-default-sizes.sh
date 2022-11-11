98240a2a90cfb201d7ad0f1eb821a5638bca541a
Evan Overman <evanrileyoverman@gmail.com>
2022-11-11 10:25:39 -0800

    adjust zathura start size

--- floating-window-default-sizes.sh
+++ floating-window-default-sizes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 bspc rule -a "kitty" center=true rectangle=920x640+0+0
-bspc rule -a "Zathura" center=true rectangle=846x980+0+0
+bspc rule -a "Zathura" center=true rectangle=691x954+0+0
 bspc rule -a "miniplayer" center=true rectangle=486x732+0+0
 bspc rule -a "simplest-fetch" center=true rectangle=780x430+0+0
 
