afb2780b5eb28e23c69b3d8d995d6d4559d19406
Evan Overman <evanrileyoverman@gmail.com>
2022-11-11 10:25:25 -0800

    better adjust padding for polybar

--- padded-mode.sh
+++ padded-mode.sh
@@ -2,18 +2,18 @@
 
 if [ "$1" == "setup" ]
 then
-	bspc config top_padding			106
+	bspc config top_padding			108
 	bspc config bottom_padding		98
 	bspc config right_padding		180
 	bspc config left_padding		180
 elif [ $(bspc config bottom_padding) == "0" ]
 then
-	bspc config top_padding			106
+	bspc config top_padding			108
 	bspc config bottom_padding		80
 	bspc config right_padding		180
 	bspc config left_padding		180
 else
-	bspc config top_padding			26
+	bspc config top_padding			28
 	bspc config bottom_padding		0
 	bspc config right_padding		0
 	bspc config left_padding		0
