b52d138b2570137bf2928ef31670c9cae24a0e68
Evan Overman <evanrileyoverman@gmail.com>
2022-11-11 10:25:13 -0800

    make fzfmenu always float

--- toggle-window-start-state.sh
+++ toggle-window-start-state.sh
@@ -4,6 +4,7 @@ if [ "$1" == "setup" ]
 then
 	bspc rule -r "*"
 	bspc rule -a "*" state=tiled
+	bspc rule -a "fzfmenu" state=floating center=true rectangle=800x440+0+0
 	exit
 fi
 
@@ -12,12 +13,14 @@ bspc rule -l | grep "*:*:* => state=tiled" > /dev/null
 if [ "$?" == "0" ]
 then
 	bspc rule -r "*"
-	bspc rule -a "*" state=floating center=true
+	bspc rule -a "*" state=floating
+	bspc rule -a "fzfmenu" center=true rectangle=800x440+0+0
 	$HOME/.config/bspwm/floating-window-default-sizes.sh
 	#notify-send "Default window state changed" "Windows will now start floating" -a "bspwm"
 else
 	bspc rule -r "*"
 	bspc rule -a "*" state=tiled
+	bspc rule -a "fzfmenu" state=floating center=true rectangle=800x440+0+0
 	$HOME/.config/bspwm/floating-window-default-sizes.sh
 	#notify-send "Default window state changed" "Windows will now start tiled" -a "bspwm"
 fi
