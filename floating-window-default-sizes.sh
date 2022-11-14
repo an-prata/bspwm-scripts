c768115d9eedc588109eeccb584099b350926f89
Evan Overman <evanrileyoverman@gmail.com>
2022-11-13 19:55:44 -0800

    change neovim to helix

--- floating-window-default-sizes.sh
+++ floating-window-default-sizes.sh
@@ -4,4 +4,5 @@ bspc rule -a "kitty" center=true rectangle=920x640+0+0
 bspc rule -a "Zathura" center=true rectangle=691x954+0+0
 bspc rule -a "miniplayer" center=true rectangle=486x732+0+0
 bspc rule -a "simplest-fetch" center=true rectangle=780x430+0+0
+bspc rule -a "helix" center=true rectangle=900x990+0+0
 
