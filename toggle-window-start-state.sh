2dc6571d71b23412713ef03cc649774c983062d7
Evan Overman <evanrileyoverman@gmail.com>
2022-11-21 15:43:33 -0700

    temporarily save window state for bspwm restarts

--- toggle-window-start-state.sh
+++ toggle-window-start-state.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/bash
 
-if [ "$1" == "setup" ]
-then
-	bspc rule -r "*"
-	bspc rule -a "*" state=tiled
-	bspc rule -a "fzfmenu" state=floating center=true rectangle=800x440+0+0
-	exit
-fi
-
-bspc rule -l | grep "*:*:* => state=tiled" > /dev/null
-
-if [ "$?" == "0" ]
-then
+set_floating() {
 	bspc rule -r "*"
 	bspc rule -a "*" state=floating
 	bspc rule -a "fzfmenu" center=true rectangle=800x440+0+0
 	$HOME/.config/bspwm/floating-window-default-sizes.sh
+	echo "floating" > "/tmp/bspwm-window-state"
 	#notify-send "Default window state changed" "Windows will now start floating" -a "bspwm"
-else
+}
+
+set_tiling () {
 	bspc rule -r "*"
 	bspc rule -a "*" state=tiled
 	bspc rule -a "fzfmenu" state=floating center=true rectangle=800x440+0+0
 	$HOME/.config/bspwm/floating-window-default-sizes.sh
+	echo "tiling" > "/tmp/bspwm-window-state"
 	#notify-send "Default window state changed" "Windows will now start tiled" -a "bspwm"
+}
+
+if [ "$1" == "setup" ]; then
+	LAST_STATE="$(cat /tmp/bspwm-window-state)"
+	
+	if [ "$LAST_STATE" == "floating"]; then
+		set_floating
+	else
+		set_tiling
+	fi
+fi
+
+LAST_STATE="$(cat /tmp/bspwm-window-state)"
+
+if [ "$LAST_STATE" == "tiling" ]; then
+	set_floating
+else
+	set_tiling
 fi
 
