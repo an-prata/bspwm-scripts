f425b3806e94120d41cf47547876aab6f624b5f2
Evan Overman <evanrileyoverman@gmail.com>
2023-01-21 14:55:26 -0800

    adjust padding

--- padded-mode.sh
+++ padded-mode.sh
@@ -2,19 +2,19 @@
 
 if [ "$1" == "setup" ]
 then
-	bspc config top_padding			108
-	bspc config bottom_padding		98
-	bspc config right_padding		180
-	bspc config left_padding		180
+	bspc config top_padding			48
+	bspc config bottom_padding	20
+	bspc config right_padding		200
+	bspc config left_padding		200
 elif [ $(bspc config bottom_padding) == "0" ]
 then
-	bspc config top_padding			108
-	bspc config bottom_padding		80
-	bspc config right_padding		180
-	bspc config left_padding		180
+	bspc config top_padding			48
+	bspc config bottom_padding	20
+	bspc config right_padding		200
+	bspc config left_padding		200
 else
 	bspc config top_padding			28
-	bspc config bottom_padding		0
+	bspc config bottom_padding	0
 	bspc config right_padding		0
 	bspc config left_padding		0
 fi
