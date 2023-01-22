7bc24f2bf285f09d3b001e473f1af95c0de86eb3
Evan Overman <evanrileyoverman@gmail.com>
2023-01-21 16:10:20 -0800

    imma comment out the jank background

--- dashboard.sh
+++ dashboard.sh
@@ -4,7 +4,7 @@ bspc rule -a "dash-miniplayer" state=floating rectangle=486x732+1131+174
 bspc rule -a "dash-simplest-fetch" state=floating rectangle=776x466+329+440
 bspc rule -a "dash-tty-clock" state=floating rectangle=320x240+786+174
 bspc rule -a "dash-cava" state=floating rectangle=429x240+329+174
-bspc rule -a "dash-back" state=floating rectangle=1920x1080+0+0
+# bspc rule -a "dash-back" state=floating rectangle=1920x1080+0+0
 
 if [ "$(cat /tmp/dash-state)" == "open" ]; then
     bspc desktop -f "$(cat /tmp/dash-desk)"
@@ -14,7 +14,7 @@ if [ "$(cat /tmp/dash-state)" == "open" ]; then
     pkill -f "kitty --class dash-simplest-fetch"
     pkill -f "kitty --class dash-tty-clock"
     pkill -f "kitty --class dash-cava"
-    pkill -f "kitty --class dash-back"
+    # pkill -f "kitty --class dash-back"
     exit
 fi
 
@@ -22,8 +22,8 @@ echo "open" > /tmp/dash-state
 echo "$(bspc query -D -d focused --names)" > /tmp/dash-desk
 bspc desktop -f '^9'
 
-kitty --class dash-back --override background=#cca37a -e ~/.config/bspwm/hold.sh &
-sleep 0.1
+# kitty --class dash-back --override background=#cca37a -e ~/.config/bspwm/hold.sh &
+# sleep 0.1
 kitty --class dash-miniplayer -e miniplayer &
 kitty --class dash-simplest-fetch -e simplest-fetch &
 kitty --class dash-tty-clock -e tty-clock -t -C 3 &
