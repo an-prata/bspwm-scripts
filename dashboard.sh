54d98144c813a0195d5d879a51c4c0a4c8aecdb4
Evan Overman <evanrileyoverman@gmail.com>
2023-01-25 13:10:05 -0800

    change simplest-fetch to calcure

--- dashboard.sh
+++ dashboard.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 bspc rule -a "dash-miniplayer" state=floating rectangle=486x732+1131+174
-bspc rule -a "dash-simplest-fetch" state=floating rectangle=776x466+329+440
+bspc rule -a "dash-calcure" state=floating rectangle=776x466+329+440
 bspc rule -a "dash-tty-clock" state=floating rectangle=320x240+786+174
 bspc rule -a "dash-cava" state=floating rectangle=429x240+329+174
 # bspc rule -a "dash-back" state=floating rectangle=1920x1080+0+0
@@ -12,7 +12,7 @@ if [ "$(cat /tmp/dash-state)" == "open" ]; then
     rm /tmp/dash-desk
     rm /tmp/dash-state
     pkill -f "kitty --class dash-miniplayer"
-    pkill -f "kitty --class dash-simplest-fetch"
+    pkill -f "kitty --class dash-calcure"
     pkill -f "kitty --class dash-tty-clock"
     pkill -f "kitty --class dash-cava"
     polybar-msg cmd show
@@ -33,7 +33,7 @@ set-wallpaper --blur
 # sleep is for animation type thing
 kitty --class dash-miniplayer -e miniplayer &
 sleep 0.1
-kitty --class dash-simplest-fetch -e simplest-fetch &
+kitty --class dash-calcure -e calcure &
 sleep 0.1
 kitty --class dash-tty-clock -e tty-clock -t -C 3 &
 sleep 0.1
