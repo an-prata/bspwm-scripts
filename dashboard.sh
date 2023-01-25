997df2e11d498e3aa6d2c57c964ce09863b2c8d5
Evan Overman <evanrileyoverman@gmail.com>
2023-01-25 14:45:22 -0800

    switch out tty-clock for peaclock

--- dashboard.sh
+++ dashboard.sh
@@ -2,7 +2,7 @@
 
 bspc rule -a "dash-miniplayer" state=floating rectangle=486x732+1131+174
 bspc rule -a "dash-calcure" state=floating rectangle=776x466+329+440
-bspc rule -a "dash-tty-clock" state=floating rectangle=320x240+786+174
+bspc rule -a "dash-peaclock" state=floating rectangle=320x240+786+174
 bspc rule -a "dash-cava" state=floating rectangle=429x240+329+174
 # bspc rule -a "dash-back" state=floating rectangle=1920x1080+0+0
 
@@ -13,7 +13,7 @@ if [ "$(cat /tmp/dash-state)" == "open" ]; then
     rm /tmp/dash-state
     pkill -f "kitty --class dash-miniplayer"
     pkill -f "kitty --class dash-calcure"
-    pkill -f "kitty --class dash-tty-clock"
+    pkill -f "kitty --class dash-peaclock"
     pkill -f "kitty --class dash-cava"
     polybar-msg cmd show
     # pkill -f "kitty --class dash-back"
@@ -35,8 +35,13 @@ kitty --class dash-miniplayer -e miniplayer &
 sleep 0.1
 kitty --class dash-calcure -e calcure &
 sleep 0.1
-kitty --class dash-tty-clock -e tty-clock -t -C 3 &
+kitty --class dash-peaclock -e peaclock &
 sleep 0.1
 kitty --class dash-cava -e cava &
-sleep 0.1
+
+# this figure has been adjusted for my machine, may need to be readjusted
+sleep 0.4
+
+# switch focus from cava to colcure
+bspc node -f south
 
