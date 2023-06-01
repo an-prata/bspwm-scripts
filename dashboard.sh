5dccd50a2805bbb14e170184deaf5d29b77ddf44
Evan Overman <evanrileyoverman@gmail.com>
2023-06-01 11:50:58 -0700

    use simplest-fetch instead of calcure which is currently broken

--- dashboard.sh
+++ dashboard.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 bspc rule -a "dash-miniplayer" state=floating rectangle=486x732+1131+174
-bspc rule -a "dash-calcure" state=floating rectangle=776x466+329+440
+bspc rule -a "dash-fetch" state=floating rectangle=776x466+329+440
 bspc rule -a "dash-peaclock" state=floating rectangle=320x240+786+174
 bspc rule -a "dash-cava" state=floating rectangle=429x240+329+174
 # bspc rule -a "dash-back" state=floating rectangle=1920x1080+0+0
@@ -12,7 +12,7 @@ if [ "$(cat /tmp/dash-state)" == "open" ]; then
     rm /tmp/dash-desk
     rm /tmp/dash-state
     pkill -f "kitty --class dash-miniplayer"
-    pkill -f "kitty --class dash-calcure"
+    pkill -f "kitty --class dash-fetch"
     pkill -f "kitty --class dash-peaclock"
     pkill -f "kitty --class dash-cava"
     polybar-msg cmd show
@@ -35,7 +35,7 @@ set-wallpaper --blur
 # sleep is for animation type thing
 kitty --class dash-miniplayer -e miniplayer &
 sleep 0.1
-kitty --class dash-calcure -e calcure &
+kitty --class dash-fetch -e simplest-fetch &
 sleep 0.1
 kitty --class dash-peaclock -e peaclock &
 sleep 0.1
