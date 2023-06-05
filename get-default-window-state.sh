39cd67cdf919d4be70028d512e305d5dc2490fe8
Evan Overman <evanrileyoverman@gmail.com>
2023-06-04 23:00:22 -0700

    polybar module improvements

--- get-default-window-state.sh
+++ get-default-window-state.sh
@@ -3,11 +3,28 @@
 # Intended for use as a polybar module in conjunction
 # with the other scripts found in this directory.
 
+OUT=""
+
+if [ $(bspc config window_gap) == "0" ]
+then
+	if [ $(bspc config bottom_padding) == "0" ]
+	then
+		OUT=""
+	else
+		OUT="p"
+	fi
+elif [ $(bspc config bottom_padding) == "0" ]
+then
+	OUT="g"
+else
+	OUT="g, p"
+fi
+
 bspc rule -l | grep "*:*:* => state=tiled" > /dev/null
 
 if [ "$?" == "0" ]
 then
-	echo "Tiling"
+	echo "Tiling   $OUT"
 else
-	echo "Floating"
+	echo "Floating  "
 fi
