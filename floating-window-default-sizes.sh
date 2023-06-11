e20bbbcbc1e98caa528fd7ad448b6ac8237047c3
Evan Overman <evanrileyoverman@gmail.com>
2023-06-11 12:13:59 -0700

    increase helix size

--- floating-window-default-sizes.sh
+++ floating-window-default-sizes.sh
@@ -4,6 +4,6 @@ bspc rule -a "kitty" center=true rectangle=920x640+0+0
 bspc rule -a "Zathura" center=true rectangle=691x954+0+0
 bspc rule -a "miniplayer" center=true rectangle=486x732+0+0
 bspc rule -a "simplest-fetch" center=true rectangle=780x430+0+0
-bspc rule -a "helix" center=true rectangle=800x800+0+0
+bspc rule -a "helix" center=true rectangle=860x1000+0+0
 bspc rule -a "tty-clock" center=true rectangle=320x240+0+0
 
